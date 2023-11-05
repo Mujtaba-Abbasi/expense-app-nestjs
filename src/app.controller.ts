adce5b3376a62f48bc091249798d241fecd225d5
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2023-11-05 14:30:44 +0500

    Extract API logic in seperate service file

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,28 +7,23 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
-import { ReportType, data } from './Data';
-import { v4 as uuid } from 'uuid';
+import { ReportType } from './Data';
+import { AppService } from './app.service';
 
 @Controller('report/:type')
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get()
   getAllReports(@Param('type') type: string) {
     const reportType =
       type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-    return data.report.filter((item) => item.type === reportType);
+    return this.appService.getAllReports(reportType);
   }
 
   @Get(':id')
-  getReportById(@Param('type') type: string, @Param('id') id: string) {
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-
-    const reportData = data.report
-      .filter((item) => item.type === reportType)
-      .find((item) => item.id === id);
-
-    return reportData || 'No records found with the provided Id';
+  getReportById(@Param('id') id: string) {
+    return this.appService.getReportById(id);
   }
 
   @Post()
@@ -36,70 +31,22 @@ export class AppController {
     @Body() body: { amount: number; source: string; type: ReportType },
     @Param('type') reportType: string,
   ) {
-    const { amount, source, type } = body;
+    const type =
+      reportType === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
 
-    if (!amount) return 'Amount is required.';
-    if (!source) return 'Source is required.';
-    if (!type) return 'Amount is required.';
-
-    if (type !== ReportType.EXPENSE && type !== ReportType.INCOME)
-      return 'Invalid Report type. Report type can either be income or expense.';
-
-    const newReport = {
-      id: uuid() as string,
-      source,
-      amount,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      type: reportType === 'expense' ? ReportType.EXPENSE : ReportType.INCOME,
-    };
-
-    data.report.push(newReport);
-
-    return newReport;
+    return this.appService.createReport(type, body);
   }
 
   @Put(':id')
   updateReport(
     @Body() body: { amount: number; source: string; type: ReportType },
-    @Param('type') reportType: string,
     @Param('id') id: string,
   ) {
-    const { type } = body;
-
-    if (!id) return 'Id is required.';
-    if (type !== ReportType.EXPENSE && type !== ReportType.INCOME)
-      return 'Invalid Report type. Report type can either be income or expense.';
-
-    const _data = data.report.find((item) => item.id === id);
-
-    if (_data) {
-      const newReport = {
-        ..._data,
-        ...body,
-      };
-
-      data.report.push(newReport);
-
-      return newReport;
-    } else return 'No Record found with the corresponding id';
+    return this.appService.updateReport(body, id);
   }
 
   @Delete(':id')
-  deleteReport(@Param('type') type: string, @Param('id') id: string) {
-    if (!id) return 'Id is required.';
-    const reportType =
-      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
-
-    const reportData = data.report
-      .filter((item) => item.type === reportType)
-      .find((item) => item.id === id);
-
-    if (reportData) {
-      const newData = data.report.filter((item) => item.id !== id);
-      data.report = [...newData];
-
-      return 'Record was deleted successfully!';
-    } else return 'No record found with the provided id.';
+  deleteReport(@Param('id') id: string) {
+    return this.appService.deleteReport(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { ReportType, data } from './Data';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAllReports(type: ReportType) {
+    return data.report.filter((item) => item.type === type);
+  }
+
+  getReportById(id: string) {
+    const reportData = data.report.find((item) => item.id === id);
+
+    return reportData || 'No records found with the provided Id';
+  }
+
+  createReport(
+    reportType: ReportType,
+    body: { amount: number; source: string; type: ReportType },
+  ) {
+    const { amount, source, type } = body;
+
+    if (!amount) return 'Amount is required.';
+    if (!source) return 'Source is required.';
+    if (!type) return 'Amount is required.';
+
+    if (type !== ReportType.EXPENSE && type !== ReportType.INCOME)
+      return 'Invalid Report type. Report type can either be income or expense.';
+
+    const newReport = {
+      id: uuid() as string,
+      source,
+      amount,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      type: reportType === 'expense' ? ReportType.EXPENSE : ReportType.INCOME,
+    };
+
+    data.report.push(newReport);
+
+    return newReport;
+  }
+
+  updateReport(
+    body: { amount: number; source: string; type: ReportType },
+    id: string,
+  ) {
+    if (!id) return 'Id is required.';
+
+    if (body.type !== ReportType.EXPENSE && body.type !== ReportType.INCOME)
+      return 'Invalid Report type. Report type can either be income or expense.';
+
+    const _data = data.report.find((item) => item.id === id);
+
+    if (!_data) {
+      return 'No Record found with the corresponding id';
+    }
+    const newReport = {
+      ..._data,
+      ...body,
+    };
+
+    data.report.push(newReport);
+
+    return newReport;
+  }
+
+  deleteReport(id: string) {
+    if (!id) return 'Id is required.';
+
+    const reportData = data.report.find((item) => item.id === id);
+
+    if (!reportData) {
+      return 'No record found with the provided id.';
+    }
+
+    const newData = data.report.filter((item) => item.id !== id);
+
+    data.report = [...newData];
+
+    return 'Record was deleted successfully!';
   }
 }
